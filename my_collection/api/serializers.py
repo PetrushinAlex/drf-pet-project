f71c7663086588579a2ca3c73e2c8df301530027
PetrushinAlex <156915643+PetrushinAlex@users.noreply.github.com>
2025-02-23 14:08:45 +0400

    Update serializers.py
    Base for serializers

--- my_collection/api/serializers.py
+++ my_collection/api/serializers.py
@@ -1,1 +1,17 @@
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
+from content.models import Game, Movie, Book
+
+
+class GameSerializer(serializers.ModelSerializer):
+  pass
+
+
+class MovieSerializer(serializers.ModelSerializer):
+  pass
+
+
+class BookSerializer(serializers.ModelSerializer):
+  pass
