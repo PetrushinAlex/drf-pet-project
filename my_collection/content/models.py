e8e17b894c5fe5006eba502aed0d67a7db246613
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-02-25 19:29:15 +0300

    Model Game

--- my_collection/content/models.py
+++ my_collection/content/models.py
@@ -2,7 +2,23 @@ from django.db import models
 
 
 class Game(models.Model):
-    pass
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название игры',
+        help_text='Введите название',
+    )
+
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name='Описание',
+        help_text='Опишите игру',
+    )
+
+    year = models.SmallIntegerField(
+        verbose_name='Дата выпуска',
+        help_text='Укажите, когда вышла игра',
+    )
 
 
 class Movie(models.Model):
