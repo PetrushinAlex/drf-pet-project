debc32f62be9104657d21e63cfca6f2b0af5018a
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-02-27 17:45:19 +0300

    Genre

--- my_collection/content/models.py
+++ my_collection/content/models.py
@@ -3,6 +3,28 @@ from django.db import models
 from content.validators import validate_release_date
 
 
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название',
+        help_text='Введите название жанра',
+    )
+    slug = models.SlugField(
+        max_length=28,
+        unique=True,
+        verbose_name='Идентификатор',
+        help_text='Введите идентификатор жанра',
+    )
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return f"Genre: {self.name} (slug: {self.slug})"
+
+
 class Game(models.Model):
     name = models.CharField(
         max_length=256,
